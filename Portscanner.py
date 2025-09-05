d506435267ab3ff1a155d68af03304992e9451f3
ViM1C <cyberborton@gmail.com>
2025-09-05 09:15:18 -0400

    Add files via upload

---
+++ Portscanner.py
@@ -0,0 +1,24 @@
+import socket
+import termcolor
+
+def scan(target, ports):
+    for port in range(1, ports):
+            scan_port(target, port)
+
+def scan_port(ipaddress, port):
+    try:
+            sock = socket.socket()
+            sock.connect((ipaddress, port))
+            print("[+] Port Opened " + str(port))
+            sock.close()
+    except:
+            print("[+] Port Closed" + str(port))
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ")
+ports = input("[*] Enter How Many Ports You Want to Scan: ")
+if ',' in targets:
+        print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+        for ip_addr in targets.split(','):
+                scan(ip_addr.strip(' '), ports)
+else:
+        scan(targets, ports)
